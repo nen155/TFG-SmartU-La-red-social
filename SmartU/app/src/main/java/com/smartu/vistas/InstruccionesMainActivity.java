4257f2699caf27480bebf4eb7fae15976087aa91
nen155 <nen156@gmail.com>
2017-06-02 17:37:21 +0200

    Añadidas las pantallas de instrucciones, falta mensajería y intereses

--- SmartU/app/src/main/java/com/smartu/vistas/InstruccionesMainActivity.java
+++ SmartU/app/src/main/java/com/smartu/vistas/InstruccionesMainActivity.java
@@ -34,8 +34,14 @@ public class InstruccionesMainActivity extends AppIntro2 {
             comentarios = bundle.getParcelableArrayList("comentarios");
         }
         //Añado los Slide para las instrucciones del Main
-        //addSlide(AppIntroFragment.newInstance(getString(R.string.menu_main), getString(R.string.descripcion_instrucciones_main), R.drawable.menu_lateral_instrucciones, ContextCompat.getColor(getApplicationContext(), R.color.colorPrimaryLight));
-        addSlide(AppIntroFragment.newInstance(getString(R.string.menu_lateral), getString(R.string.descripcion_instrucciones_menu), R.drawable.menu_lateral_instrucciones, Color.parseColor("#2196F3")));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.menu_lateral), getString(R.string.descripcion_instrucciones_menu), R.mipmap.pagina10, Color.parseColor("#29b6f6")));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.muro), getString(R.string.descripcion_instrucciones_main), R.mipmap.pagina7, Color.parseColor("#29b6f6")));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.filtro), getString(R.string.descripcion_instrucciones_filtro), R.mipmap.pagina9, Color.parseColor("#29b6f6")));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.acciones), getString(R.string.descripcion_instrucciones_buenaidea), R.mipmap.pagina2, Color.parseColor("#29b6f6")));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.comenta), getString(R.string.descripcion_instrucciones_comentarios), R.mipmap.pagina3, Color.parseColor("#29b6f6")));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.informacion), getString(R.string.descripcion_instrucciones_informacion), R.mipmap.pagina2, Color.parseColor("#29b6f6")));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.elementos), getString(R.string.descripcion_instrucciones_elemento), R.mipmap.pagina1, Color.parseColor("#29b6f6")));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.chat), getString(R.string.descripcion_instrucciones_chat), R.mipmap.pagina6, Color.parseColor("#29b6f6")));
         //Muestro el botón para saltarme las instrucciones
         showSkipButton(true);
         //Muestro un botón para pasar de slide
