c88e6177ba821b50b67996f2987420ea852fc86f
nen155 <nen156@gmail.com>
2017-07-10 16:37:12 +0200

    Eliminados backup

--- Server/smartu/app/lib/database.php
+++ Server/smartu/app/lib/database.php
@@ -7,7 +7,7 @@ class Database
 {
     public static function StartUp()
     {
-        $pdo = new PDO('mysql:host=db684697551.db.1and1.com;dbname=db684697551;charset=utf8', 'dbo684697551', 'TFGInter1617!');
+        $pdo = new PDO('mysql:host=TU_HOST;dbname=TU_BD;charset=utf8', 'USUARIO', 'PASSWORD');
         
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
